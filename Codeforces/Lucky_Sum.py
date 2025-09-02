55a6b1b1fcba238475080776d8a3f323b7c5f46a
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-09-02 20:04:34 +0300

    Update Lucky_Sum.py
    Cleared up my logic

--- Codeforces/Lucky_Sum.py
+++ Codeforces/Lucky_Sum.py
@@ -1,12 +1,11 @@
 sum = int(input())
-# n(7+4) = sum
+# Noticed that n(7+4) = sum, then you start with 4s and then 7s
 
 if sum % 11 != 0: # if its not divisible then that number DNE
     print(-1)
 
 n = sum / 11  # solve for n
 
-# n=2
 lucky = ""
 for i in range(int(n)):
     lucky = "4" * (i + 1) + "7" * (i + 1)
