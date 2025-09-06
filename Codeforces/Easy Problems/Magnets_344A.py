66d1d42e8709b19080ee8fd30b83dbe55fe3931f
Faisal Alabdullateef <falabdullateef@gmail.com>
2025-09-06 22:32:32 +0300

    Add Magnets_344A solution and .gitignore
    Added a solution for the Codeforces problem 'Magnets_344A' and created a .gitignore file to exclude VS Code settings.

---
+++ Codeforces/Easy Problems/Magnets_344A.py
@@ -0,0 +1,19 @@
+n = int(input()) # number of magnets to test
+
+possible = True
+recent = 0
+groups = 1
+
+for i in range (n):
+    current_magnet = int(input())
+    current_magnet = 1 if current_magnet == 10 else -1 # 1 if last edge is +ve, -1 if last edge is -ve
+ 
+    if i != 0: #accounting for the first iteration
+        if current_magnet != recent: #repulsion
+            groups += 1
+            recent = current_magnet
+
+    else:
+        recent = current_magnet
+
+print (groups)
