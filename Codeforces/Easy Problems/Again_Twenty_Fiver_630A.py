63cf59422a3be04f78a02aee7163c87d3a06a457
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-09-08 22:34:29 +0300

    Add solution for Again Twenty Five 630A problem
    Implements a simple solution that prints 25 for any input, based on the observation that powers of 5 greater than 1 always end with 25.

---
+++ Codeforces/Easy Problems/Again_Twenty_Fiver_630A.py
@@ -0,0 +1,4 @@
+n = int(input()) # I hate to handle an input regardless of the fact that it's always 25
+# find a pattern 5^2 =25, 5^3=125, 5^4=625 always ends with 25.
+print(25)
+# thats way too easy lol
